efa41da47d7580351141f606519ed692994e7dbd
ruisoares <rui_eduardo93@hotmail.com>
2021-10-19 22:05:54 +0100

    * Added release description param to release.sh script and applied release name format

--- tools/release/github/release.sh
+++ tools/release/github/release.sh
@@ -2,9 +2,10 @@
 username=$1
 password=$2
 tagName=$3
+description=$4
 curl \
   -u "$username:$password" \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/ruieduardosoares/Mvi-kotlin/releases \
-  -d "{\"tag_name\":\"$tagName\"}" \
+  -d "{\"tag_name\":\"$tagName\" \"name\":\"Release $tagName\", \"body\":\"$description\"}"
