b7aa6e3602580449a102873a883617ecdd89fd61
ruisoares <rui_eduardo93@hotmail.com>
2021-10-19 18:58:24 +0100

    * Created first simple release.sh code with simple curl to create release from tag

--- tools/release/github/release.sh
+++ tools/release/github/release.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+username=$1
+password=$2
+tagName=$3
+curl \
+  -u "$username:$password" \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/ruieduardosoares/Mvi-kotlin/releases \
+  -d "{\"tag_name\":\"$tagName\"}" \
