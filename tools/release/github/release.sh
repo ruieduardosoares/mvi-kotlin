3039090e174e7ee7eb84cfe8311a50e6de68b46f
ruisoares <rui_eduardo93@hotmail.com>
2021-10-19 22:27:30 +0100

    * Removed input to add release notes * Made github auto generate release notes from merged branches until tag commit

--- tools/release/github/release.sh
+++ tools/release/github/release.sh
@@ -2,11 +2,10 @@
 username=$1
 password=$2
 tagName=$3
-description=$4
 releaseName="Release $tagName"
 curl \
   -u "$username:$password" \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/ruieduardosoares/Mvi-kotlin/releases \
-  -d "{\"tag_name\":\"$tagName\", \"name\":\"$releaseName\", \"body\":\"$description\"}"
+  -d "{\"tag_name\":\"$tagName\", \"name\":\"$releaseName\", \"generate_release_notes\": true}"
