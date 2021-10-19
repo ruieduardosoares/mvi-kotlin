87aca5c348a998a37cd142443c56a8ea4e8e878a
ruisoares <rui_eduardo93@hotmail.com>
2021-10-19 22:14:45 +0100

    * Creaed releaseName variable ti release name format * Fixed json, missing comma

--- tools/release/github/release.sh
+++ tools/release/github/release.sh
@@ -3,9 +3,10 @@ username=$1
 password=$2
 tagName=$3
 description=$4
+releaseName="Release $tagName"
 curl \
   -u "$username:$password" \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/ruieduardosoares/Mvi-kotlin/releases \
-  -d "{\"tag_name\":\"$tagName\" \"name\":\"Release $tagName\", \"body\":\"$description\"}"
+  -d "{\"tag_name\":\"$tagName\", \"name\":\"$releaseName\", \"body\":\"$description\"}"
