63ecdd7b0d853f27b88b154bf3e3d856cbb0d206
ruisoares <rui_eduardo93@hotmail.com>
2021-10-25 22:23:36 +0100

    * Added Kdoc for MviView.kt public interface with some description and how to use it

--- app/src/main/kotlin/io/github/ruieduardosoares/android/mvi/kotlin/MviView.kt
+++ app/src/main/kotlin/io/github/ruieduardosoares/android/mvi/kotlin/MviView.kt
@@ -2,6 +2,30 @@ package io.github.ruieduardosoares.android.mvi.kotlin
 
 import androidx.annotation.MainThread
 
+/**
+ * This interface represents the base MVI view contract to implement.
+ *
+ * Example of an implementation would be creating a new specific view interface and then extend
+ * from MviView with the respective ViewState class.
+ *
+ * After that you just need to declare the necessary intent methods that your view will trigger
+ *
+ *
+ * Example:
+ * ```
+ * interface NewScreenView: MviView<NewScreenState>{
+ *
+ *     fun someIntent(): Observable<Unit>
+ *
+ *     fun anotherIntent(): Observable<Unit>
+ * }
+ * ```
+ *
+ * As a final step you would implement this interface in a class that has access to the
+ * Activity view for example.
+ *
+ * Remember that the Activity/Fragment are not your view, but rather a container for your it
+ */
 @MainThread
 interface MviView<T> {
 
