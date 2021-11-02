39ac6a356805c68ab130a9888511749f2794bac8
ruisoares <rui_eduardo93@hotmail.com>
2021-11-02 13:01:20 +0000

    * Created mvi sample project as a starting guide on how to use this lib

---
+++ mvi-sample/app/src/main/kotlin/com/mvi/sample/LaunchView.kt
@@ -0,0 +1,51 @@
+package com.mvi.sample
+
+import com.mvi.sample.databinding.ActivityLaunchBinding
+import io.github.ruieduardosoares.android.mvi.kotlin.MviView
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.subjects.PublishSubject
+
+class LaunchView : MviView<LaunchViewState> {
+
+    var binding: ActivityLaunchBinding? = null
+
+    private val mBinding: ActivityLaunchBinding
+        get() = binding!!
+
+    private val mAnimateLogoIntentSubject = Single.just(Unit).toObservable()
+    private val mAnimateLogoTextIntentSubject = PublishSubject.create<Unit>()
+
+    fun animateLogoIntent(): Observable<Unit> = mAnimateLogoIntentSubject.hide()
+
+    fun animateLogoTextIntent(): Observable<Unit> = mAnimateLogoTextIntentSubject.hide()
+
+    override fun render(state: LaunchViewState) {
+        when (state) {
+            LaunchViewState.AnimateLogoState -> {
+                mBinding.imageView.apply {
+                    post {
+                        mBinding.textWelcome.alpha = 0f
+                        y = mBinding.root.height.toFloat()
+                        animate()
+                            .setDuration(700)
+                            .translationY(0f)
+                            .withEndAction {
+                                mAnimateLogoTextIntentSubject.onNext(Unit)
+                            }
+                    }
+                }
+            }
+            LaunchViewState.AnimateLogoTextState -> {
+                mBinding.textWelcome.apply {
+                    post {
+                        animate().setDuration(700).alpha(1f)
+                    }
+                }
+            }
+            else -> {
+                //Nothing
+            }
+        }
+    }
+}
